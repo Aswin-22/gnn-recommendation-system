ba806502d8c762d819a9207bec2d40fd7782d99e
Aswin-22 <Aswin.luck543@gmail.com>
2025-02-08 16:41:47 +0530

    Login and Signup Page Updated

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
+import "./loginstyles.css";
+
+const LoginPage = () => {
+  return (
+    <div className="login-main-container">
+      <Header />
+      <div className="login-body">
+        <div className="container">
+          <div className="login-container">
+            <h2>Welcome back</h2>
+            <form>
+              <label>Email</label>
+              <input type="email" placeholder="example@gmail.com" />
+              <label>Password</label>
+              <input type="password" placeholder="•••••••••" />
+              <button type="submit">Log in</button>
+            </form>
+            <p className="signup-link">
+              Don't have an account? <Link to="/signup">Sign up for free</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
+import "./loginstyles.css";
+
+function SignUp() {
+  return (
+    <div>
+      <div className="login-main-container">
+        <Header />
+        <div className="login-body">
+          <div className="container">
+            <div className="login-container">
+              <h2>Welcome !!</h2>
+              <form>
+                <label>Full Name</label>
+                <input type="text" placeholder="Jhon Wick" />
+                <label>Email</label>
+                <input type="email" placeholder="example@gmail.com" />
+                <label>Password</label>
+                <input type="password" placeholder="•••••••••" />
+                <label>Confirm Password</label>
+                <input type="password" placeholder="•••••••••" />
+                <button type="submit">Sign Up</button>
+              </form>
+              <p className="signup-link">
+                Already have an account? <Link to="/login">Login</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
